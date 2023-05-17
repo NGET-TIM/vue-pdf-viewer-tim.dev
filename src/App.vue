790461dd57cc150d2c0d7e496720f26d39051ab7
NGET-TIM <95674103+NGET-TIM@users.noreply.github.com>
2023-05-17 11:01:33 +0700

    testing-pre-view

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <WebViewer :path="`${publicPath}lib`"/>
+  </div>
+</template>
+
+<script>
+import WebViewer from './components/WebViewer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WebViewer
+  },
+  data () {
+    return {
+      publicPath: "http://localhost:5173/"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
